20e143a7cab83ee5b61aac740558bce372fa5eb1
adtbo <adtmustika@gmail.com>
2020-01-27 15:01:10 +0800

    finishing challenge for 15

--- 15-LocalStorageEventDelegation/script.js
+++ 15-LocalStorageEventDelegation/script.js
@@ -1,39 +1,59 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = JSON.parse(localStorage.getItem('items')) || [] ;
+var items = JSON.parse(localStorage.getItem('items')) || [];
 
-function addList(e){
+function addList(e) {
     e.preventDefault();
-    const item ={
+    const item = {
         text: (this.querySelector('[name=item]')).value,
         checked: false
     };
     //console.log(item);
     items.push(item);
-    populateList(items,itemsList);
+    populateList(items, itemsList);
     localStorage.setItem('items', JSON.stringify(items));
     this.reset();
 };
 
 function populateList(itemList = [], displayList) {
-    displayList.innerHTML = itemList.map((plate, i) => {
-        return `
+    if (itemList.length> 0) {
+        displayList.innerHTML = itemList.map((plate, i) => {
+            return `
          <li>
             <input type="checkbox" data-index=${i} id="item${i}" ${plate.checked ? 'checked' : ''}/>
             <label for="item${i}">${plate.text}</label>
          </li>
         `;
-    }).join('');
+        }).join('');
+    }
 };
 
-function toggleDone(e){
-    if(!e.target.matches('input')) return;
+function clearList() {
+    localStorage.clear();
+    items = [];
+    itemsList.innerHTML = `<li>Loading Tapas...</li>`;
+}
+
+function checkAll() {
+    items.forEach(e => {
+        e.checked = true;
+    });
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
+function toggleDone(e) {
+    if (!e.target.matches('input')) return;
     const index = e.target.dataset.index;
     items[index].checked = !items[index].checked;
     localStorage.setItem('items', JSON.stringify(items));
-    populateList(items,itemsList);
+    populateList(items, itemsList);
 }
 
 addItems.addEventListener('submit', addList);
-itemsList.addEventListener('click',toggleDone);
-populateList(items,itemsList);
+itemsList.addEventListener('click', toggleDone);
+
+document.querySelector('#clear-list').addEventListener('click', clearList);
+document.querySelector('#check-list').addEventListener('click', checkAll);
+
+populateList(items, itemsList);
