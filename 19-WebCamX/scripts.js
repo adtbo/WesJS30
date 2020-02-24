6a901892661bc6497f3b0d7225e780fdab1f6bf5
adtbo <adtmustika@gmail.com>
2020-02-24 15:53:23 +0800

    finishing #19. should go back again to review some excerise! this was great

---
+++ 19-WebCamX/scripts.js
@@ -0,0 +1,81 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream =>{
+            //console.log(localMediaStream);
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err=> {
+            console.error('webcam not authorised to used', err);
+        })
+}
+
+function paintToCanvas(){
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        //get pixels
+        let pixels = ctx.getImageData(0, 0, width, height);
+        //apply filter
+        //pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
+        //some movement
+        ctx.globalAlpha = 0.2;
+        //put it back
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
+
+function takePhoto() {
+    //play sound
+    snap.currentTime = 0;
+    snap.play();
+
+    //take the data
+
+    const data = canvas.toDataURL('image/jpeg');
+    //console.log(data);
+
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'snap');
+    link.innerHTML = `<img src="${data}" alt="snap"/>`;
+    strip.insertBefore(link, strip.firstChild)
+}
+
+function redEffect(pixels){
+    for(let i =0; i < pixels.data.length; i+=4){
+        pixels.data[i + 0] +=100  // red
+        pixels.data[i + 1] -=50 // green
+        pixels.data[i + 2] *=0.5 // blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels){
+    for(let i =0; i < pixels.data.length; i+=4){
+        pixels.data[i - 150] = pixels.data[i + 0]  // red
+        pixels.data[i + 200] = pixels.data[i + 1] // green
+        pixels.data[i - 350] = pixels.data[i + 2] // blue
+    }
+    return pixels;
+}
+
+function  greenScreen(pixels){
+    const levels ={};
+    
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
